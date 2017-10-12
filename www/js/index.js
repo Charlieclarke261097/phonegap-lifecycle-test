e5ff34ec3b593296f17e4008fbba511dbfc3de0a
Charlieclarke261097 <clac4_16@uni.worc.ac.uk>
2017-10-12 13:53:01 +0100

    ewgtwe
    wetwet

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,7 @@
 var paused_count =0;
 var resumed_count = 0;
 var launched_count = 0;
-
+//event listner defined for device ready event
 document.addEventListener("deviceready", onDeviceReady, false);
 		
 	
@@ -13,9 +13,9 @@ function updateDisplay() {
 
 
 // device APIs are available
-//
+//Event handlers for the resume and pause events
     function onDeviceReady() {
-	
+	//Event lisntners defined
 	document.addEventListener("resume", onResume, false);
 	document.addEventListener("pause", onPause, false);
 	
@@ -25,7 +25,8 @@ function updateDisplay() {
 	alert("device ready");
     }
 
-
+    //Event handlers used as a result of the on pause event beign triggered 
+    //This adds 1 to the pause count each time someone goes out of the app
     function onPause() {
 	
 	paused_count++;
@@ -34,7 +35,8 @@ function updateDisplay() {
 	alert("pause");
     }
 	
-
+    //Event handler used as a reuslt of the on resume efent being triggered
+    //This adds 1 to the resume count each time someone goes back into the app
     function onResume() {
 		
 	resumed_count++;
